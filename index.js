85730a1ca3d211ec2e39070c8b29bfefe7067165
fran <franklynssierracontreras@gmail.com>
2022-05-20 17:13:45 -0400

    ejecrcicio botcamp

---
+++ index.js
@@ -0,0 +1,19 @@
+let myName="frankly sierra";
+
+let Age=14;
+
+if(true){
+    console.log("i am a web delevoper")
+}else{
+    console.log("i am not a web delevoper")
+}
+const now= new Date()
+
+const myAge=new Date("febrary 02 2007")
+console.log(myAge)
+const myBook={
+title:harryPotter,
+author:J.K.Rowling,
+date:1997,
+url:https//harrypotter.fandom.com/es/wiki/Harry_Potter_y_la_piedra_filosofal,
+}
